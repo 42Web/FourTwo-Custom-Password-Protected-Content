647b378fa9484413cd9ac9525b838cd45d0f2ab3
Christopher Spires <cspires@gmail.com>
2015-11-16 23:09:11 -0500

    More fourtwo_ updates

--- inc/js/functions.js
+++ inc/js/functions.js
@@ -1,11 +1,11 @@
 var $ = jQuery.noConflict();
 
-function bodas_cppt_check_visibility() {
+function fourtwo_cppt_check_visibility() {
 	
 	if ($('#visibility-radio-password').is(':checked')) {
-		$('#bodas_cppt_textmeta').attr('style', 'display: block !important');
+		$('#fourtwo_cppt_textmeta').attr('style', 'display: block !important');
 	} else {
-		$('#bodas_cppt_textmeta').attr('style', 'display: none !important');
+		$('#fourtwo_cppt_textmeta').attr('style', 'display: none !important');
 	}
 	
 	console.log('test');
@@ -17,11 +17,11 @@ function bodas_cppt_check_visibility() {
 	"use strict";
 	
 	// Show/Hide Metabox on page load
-	bodas_cppt_check_visibility();
+	fourtwo_cppt_check_visibility();
 	
 	// Show/Hide Metabox on visibility select
 	$('#post-visibility-select input[type="radio"]').on('click', function() {
-		bodas_cppt_check_visibility();
+		fourtwo_cppt_check_visibility();
 	});
 	
 	
--- inc/metabox/password-text.php
+++ inc/metabox/password-text.php
@@ -3,60 +3,60 @@
  * Add Metabox
  */
  
-function bodas_cppc_add_metabox() {
+function fourtwo_cppc_add_metabox() {
 	
 	global $_wp_post_type_features;
-	add_meta_box( 'bodas_cppc_textmeta', 'Custom Password Protect Page Content', 'bodas_cppc_metabox_cb', null, 'normal', 'high' );
+	add_meta_box( 'fourtwo_cppc_textmeta', 'Custom Password Protect Page Content', 'fourtwo_cppc_metabox_cb', null, 'normal', 'high' );
 
 }
-add_action( 'add_meta_boxes', 'bodas_cppc_add_metabox' );
+add_action( 'add_meta_boxes', 'fourtwo_cppc_add_metabox' );
 
 
 /**
  * Metabox Callback
  */
  
-function bodas_cppc_metabox_cb($post) {
+function fourtwo_cppc_metabox_cb($post) {
 	
 	global $post;
 	
 	$values = get_post_custom($post->ID);
 	
-	if (isset($values['bodas_cppc_text_string'])) {
-		$editor_content = $values['bodas_cppc_text_string'][0];
+	if (isset($values['fourtwo_cppc_text_string'])) {
+		$editor_content = $values['fourtwo_cppc_text_string'][0];
 	} else {
 		$editor_content = null;
 	}
 	
 	$editor_settings = array(
 		'media_buttons' => false,
-		'textarea_name' => 'bodas_cppc_text_string'
+		'textarea_name' => 'fourtwo_cppc_text_string'
 	);
 	
-	echo '<p class="bodas_cppc_metabox_desc">' . __('If you would like to override the default WordPress password protected content, or the content specified in this plugins global options, simply fill out the editor below.', 'bodas_cppc') . '</p>';
+	echo '<p class="fourtwo_cppc_metabox_desc">' . __('If you would like to override the default WordPress password protected content, or the content specified in this plugins global options, simply fill out the editor below.', 'fourtwo_cppc') . '</p>';
 	
-	wp_editor(htmlspecialchars_decode($editor_content), 'bodas_cppc_text_string', $editor_settings);
+	wp_editor(htmlspecialchars_decode($editor_content), 'fourtwo_cppc_text_string', $editor_settings);
 	
-	wp_nonce_field( 'bodas_cppc_meta_box_nonce', 'meta_box_nonce' );
+	wp_nonce_field( 'fourtwo_cppc_meta_box_nonce', 'meta_box_nonce' );
 	
 }
 
 
-function bodas_cppc_metabox_save($post_id) {
+function fourtwo_cppc_metabox_save($post_id) {
 	
 	// bail if we're doing an auto save
 	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
 
 	// if our nonce isn't there, or we can't verify it, bail
-	if(!isset($_POST['meta_box_nonce']) || !wp_verify_nonce($_POST['meta_box_nonce'], 'bodas_cppc_meta_box_nonce')) return;
+	if(!isset($_POST['meta_box_nonce']) || !wp_verify_nonce($_POST['meta_box_nonce'], 'fourtwo_cppc_meta_box_nonce')) return;
 
 	// if our current user can't edit this post, bail
 	if(!current_user_can('edit_post')) return;
 
 	// make sure your data is set before trying to save it
-	if( isset( $_POST['bodas_cppc_text_string'] ) )
-		update_post_meta( $post_id, 'bodas_cppc_text_string', htmlspecialchars($_POST['bodas_cppc_text_string']) );
+	if( isset( $_POST['fourtwo_cppc_text_string'] ) )
+		update_post_meta( $post_id, 'fourtwo_cppc_text_string', htmlspecialchars($_POST['fourtwo_cppc_text_string']) );
 	
 }
-add_action('save_post', 'bodas_cppc_metabox_save');
-add_action('plugins_loaded', 'bodas_cppc_metabox_save');
+add_action('save_post', 'fourtwo_cppc_metabox_save');
+add_action('plugins_loaded', 'fourtwo_cppc_metabox_save');
